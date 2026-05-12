c75b3bdb76bb2dc7c2380b8880e939b59aefc602
Mikko Sanera <rns.jey@gmail.com>
2026-05-12 19:13:39 +0100

    Add authentication middleware

---
+++ proxy.ts
@@ -0,0 +1,13 @@
+import { auth } from "@/lib/auth/server";
+
+export default auth.middleware({
+  // Redirects unauthenticated users to sign-in page
+  loginUrl: "/sign-in",
+});
+
+export const config = {
+  matcher: [
+    // Protected routes requiring authentication
+    "/",
+  ],
+};
