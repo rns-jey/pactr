f918411cb4585c41bd41f5d7b978707eade05fb9
Mikko Sanera <rns.jey@gmail.com>
2026-05-12 18:07:23 +0100

    Build create a group ui and flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,78 @@
+"use client";
+
 import { Button } from "@/components/atoms/Button";
 import Input from "@/components/atoms/Input";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/molecules/Card";
 import { Field, FieldLabel } from "@/components/organisms/Field";
 import { UserPlus, UsersIcon } from "lucide-react";
+import { useState } from "react";
 
 export default function Home() {
+  const [action, setAction] = useState<"create" | "join" | "">("");
+
   return (
     <div className="p-4 min-h-screen flex flex-col items-center justify-center gap-4">
-      <div>
-        <h2 className="font-semibold text-center text-2xl">Welcome, User!</h2>
-        <p className="text-center text-muted-foreground">Let&apos;s get you connected with an accountability partner</p>
-      </div>
-
-      <Card className="cursor-pointer hover:bg-muted w-full min-h-24 max-w-sm">
-        <CardContent className="flex items-center gap-2">
+      {action === "" && (
+        <>
           <div>
-            <UsersIcon className="w-10 h-10" />
+            <h2 className="font-semibold text-center text-2xl">Welcome, User!</h2>
+            <p className="text-center text-muted-foreground">
+              Let&apos;s get you connected with an accountability partner
+            </p>
           </div>
 
-          <div>
-            <h3 className="font-semibold text-base">Create a Group</h3>
-            <p>Start a new accountability group and invite your friends to join you on your journey.</p>
-          </div>
-        </CardContent>
-      </Card>
+          <Card className="cursor-pointer hover:bg-muted w-full min-h-24 max-w-sm" onClick={() => setAction("create")}>
+            <CardContent className="flex items-center gap-2">
+              <div>
+                <UsersIcon className="w-10 h-10" />
+              </div>
 
-      <Card className="cursor-pointer hover:bg-muted w-full min-h-24 max-w-sm">
-        <CardContent className="flex items-center gap-2 my-auto">
-          <div>
-            <UserPlus className="w-10 h-10" />
-          </div>
+              <div>
+                <h3 className="font-semibold text-base">Create a Group</h3>
+                <p>Start a new accountability group and invite your friends to join you on your journey.</p>
+              </div>
+            </CardContent>
+          </Card>
 
-          <div className="">
-            <h3 className="font-semibold text-base">Join a Group</h3>
-            <p>Enter a code to join an existing accountability group.</p>
-          </div>
-        </CardContent>
-      </Card>
+          <Card className="cursor-pointer hover:bg-muted w-full min-h-24 max-w-sm" onClick={() => setAction("join")}>
+            <CardContent className="flex items-center gap-2 my-auto">
+              <div>
+                <UserPlus className="w-10 h-10" />
+              </div>
 
-      <Button variant={"outline"} className="w-full max-w-sm">
-        Sign out
-      </Button>
+              <div className="">
+                <h3 className="font-semibold text-base">Join a Group</h3>
+                <p>Enter a code to join an existing accountability group.</p>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Button variant={"outline"} className="w-full max-w-sm">
+            Sign out
+          </Button>
+        </>
+      )}
+
+      {action === "create" && (
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Create a Group</CardTitle>
+            <CardDescription>Name your accountability group.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Field>
+              <FieldLabel>Group Name</FieldLabel>
+              <Input placeholder="eg. Fitness Buddies, Study Group" />
+            </Field>
+          </CardContent>
+          <CardFooter className="flex gap-2 relative">
+            <Button variant={"outline"} className="flex-1" onClick={() => setAction("")}>
+              Back
+            </Button>
+            <Button className="flex-1">Create Group</Button>
+          </CardFooter>
+        </Card>
+      )}
     </div>
   );
 }
