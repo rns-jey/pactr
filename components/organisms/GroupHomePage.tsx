ebde3e8c2dc00a7370539701f2bedbe519c56537
Mikko Sanera <rns.jey@gmail.com>
2026-05-21 18:15:04 +0100

    Build a reusable tabs component

--- components/organisms/GroupHomePage.tsx
+++ components/organisms/GroupHomePage.tsx
@@ -1,9 +1,10 @@
-import { Clock, User, Users2 } from "lucide-react";
+import { Users2 } from "lucide-react";
 import { Card, CardContent, CardHeader, CardTitle } from "../molecules/Card";
-import { Group, Profile } from "@/lib/generated/prisma/client";
+import { Profile } from "@/lib/generated/prisma/client";
 import { GroupWithUsers } from "@/types";
 import { Button } from "../atoms/Button";
 import UserCard from "../molecules/UserCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../molecules/Tabs";
 
 interface GroupHomePageProps {
   group: GroupWithUsers;
@@ -15,37 +16,46 @@ export default function GroupHomePage({ group, profile }: GroupHomePageProps) {
 
   return (
     <main className="p-4">
-      <Card>
-        <CardHeader>
-          <CardTitle className="flex items-center gap-2">
-            <Users2 />
-            <span className="font-semibold">{name}</span>
-          </CardTitle>
-        </CardHeader>
-        <CardContent className="grid gap-4 sm:grid-cols-2">
-          {members.map((member) => (
-            <UserCard
-              key={member.id}
-              name={member.profile.name}
-              isCurrentUser={member.profileId === profile.id}
-            />
-          ))}
+      <Tabs defaultValue="workout">
+        <TabsList variant={"line"}>
+          <TabsTrigger value="workout">Workout</TabsTrigger>
+          <TabsTrigger value="group">Group</TabsTrigger>
+        </TabsList>
+        <TabsContent value="workout"></TabsContent>
+        <TabsContent value="group">
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex items-center gap-2">
+                <Users2 />
+                <span className="font-semibold">{name}</span>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="grid gap-4 sm:grid-cols-2">
+              {members.map((member) => (
+                <UserCard
+                  key={member.id}
+                  name={member.profile.name}
+                  isCurrentUser={member.profileId === profile.userId}
+                />
+              ))}
 
-          {!members && group && (
-            <div className="bg-secondary rounded-xl p-6 text-center">
-              <p className="text-muted-foreground mb-2 text-xs tracking-wider uppercase">
-                YOUR INVITE CODE
-              </p>
-              <p className="text-primary mb-4 font-mono text-3xl font-bold tracking-wider">
-                {group.code}
-              </p>
-              <Button variant={"outline"} size={"sm"}>
-                Copy code
-              </Button>
-            </div>
-          )}
-        </CardContent>
-      </Card>
+              {members.length < 2 && group && (
+                <div className="bg-secondary rounded-xl p-6 text-center">
+                  <p className="text-muted-foreground mb-2 text-xs tracking-wider uppercase">
+                    YOUR INVITE CODE
+                  </p>
+                  <p className="text-primary mb-4 font-mono text-3xl font-bold tracking-wider">
+                    {group.code}
+                  </p>
+                  <Button variant={"outline"} size={"sm"}>
+                    Copy code
+                  </Button>
+                </div>
+              )}
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </main>
   );
 }
